0808b97d23a6da0b95af7cc2cc26ee07fb18183f
Klitter <2366743949@qq.com>
2018-10-13 13:47:48 +0800

    delete out of date test and modify README

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ This program is used to generate proper path for autonomatic driving. Since path
 be simplified as skelton in this program, we need to do 2D approximately convex \
 decomposition. (This part comes from https://github.com/jmlien/acd2d) After ACD, we use \
 SOM alogrithm to generate path for TSP and MTSP problem.
+
+print_image.py: print solution to 'only_point.png' and 'path.png'
+main.py:You should input cities list in a list which contains all city postion in the \
+following way:[double/int:pos_x,double/int:pos_y], and after computing you will get a \
+sorted list which represents the order of cities in the following way: \
+[double/int:pos_x, double/int:pos_y, double/int:key].
--- main.py
+++ main.py
@@ -5,21 +5,46 @@ from neuron import init_network, get_neighborhood
 from testing import initialize, get_final_distance
 from image_process import read_image
 import data_generator
+import multiprocessing
+import time
 
 global im,mlength,mheight
 
 def select_closest(network, city,unused):
-	min = float('inf')
+	# min = float('inf')
+	d_list = []
+	while(len(d_list) != 0):
+		d_list.pop()
 	min_index = -1
+	pool = multiprocessing.Pool(processes=50)
 	for i in range(len(network)):
 		# print(get_unwall(network[i][0],network[i][1]))
 		# print((city[0],city[1]))
-		d = get_final_distance(get_unwall(network[i][0],network[i][1]),(city[0],city[1])) 
-		if d < min and unused[i]:
-			min = d
-			min_index = i
+		# d_list.append(get_final_distance(get_unwall(network[i][0],network[i][1]),(city[0],city[1])))
+		d_list.append((pool.apply_async(get_dis_in_pool, args=(network, city, i, )), i))
+	pool.close()
+	pool.join()
+#	print(len(d_list))
+	# if d < min and unused[i]:
+	# 	min = d
+	# 	min_index = i
+#	print(d_list)
+	p_list = []
+	q_list = []
+	for res in d_list:
+		p_list.append((res[0].get(),res[1]))
+	for i in range(len(p_list)):
+		q_list.append(p_list[i][0])
+	min_index = p_list.index(min(p_list))
+	min_index = d_list[min_index][1]
+	##
+	##
+	###此处改多进程
 	unused[min_index] = False 
 	return min_index
+
+def get_dis_in_pool(network,city,i):
+	return (get_final_distance(get_unwall(network[i][0],network[i][1]),(city[0],city[1])), i)
 
 def get_unwall(x,y):
 	if im[int(x)][int(y)] == 1:
@@ -41,7 +66,7 @@ def get_unwall(x,y):
 							q.put((i,k))
 
 def som(cities, epoches, learning_rate = 0.8):
-	num_neuron = len(cities) * 8
+	num_neuron = len(cities) * 12
 	network = np.array(init_network(num_neuron,im,mlength,mheight))
 	n = num_neuron
 	for i in range(epoches):
@@ -51,8 +76,8 @@ def som(cities, epoches, learning_rate = 0.8):
 			winner_idx = select_closest(network, cities[k],unused)
 			gaussian = get_neighborhood(winner_idx, n//10, num_neuron)
 			network += gaussian[:,np.newaxis] * learning_rate * (cities[k] - network)
-			learning_rate *= 0.99997
-			n = n * 0.9997
+			learning_rate *= 0.9997
+			n = n * 0.997
 		'''
 		unused = [True] * num_neuron
 		tmplist = []
@@ -76,9 +101,14 @@ def som(cities, epoches, learning_rate = 0.8):
 	return cities
 
 if __name__ == '__main__':
-	(im,mlength,mheight) = read_image('./a.pgm',2)
+	print(time.asctime(time.localtime(time.time())))
+	ratio = 2
+	(im,mlength,mheight) = read_image('./a.pgm',ratio)
 	cities = data_generator.data_generate(100,im,mlength,mheight)
-
-
 	initialize(im,mlength,mheight)
+	# cities = [[111, 151, 7], [112, 154, 17], [114, 149, 26], [118, 150, 37], [134, 150, 46], [137, 157, 57], [144, 156, 66], [141, 161, 75], [139, 167, 85], [132, 161, 94], [132, 159, 103], [130, 157, 113], [116, 158, 124], [105, 158, 133], [105, 164, 143], [98, 161, 153], [93, 158, 163], [86, 155, 172], [85, 164, 183], [77, 164, 193], [69, 170, 203], [68, 160, 213], [62, 163, 223], [61, 153, 233], [52, 149, 242], [45, 148, 252], [54, 145, 262], [62, 133, 271], [54, 120, 285], [67, 118, 296], [65, 111, 306], [54, 111, 316], [58, 108, 327], [61, 97, 336], [55, 92, 345], [63, 90, 354], [60, 87, 363], [64, 85, 372], [60, 79, 381], [50, 83, 392], [47, 83, 401], [48, 71, 412], [53, 70, 422], [52, 60, 433], [44, 45, 445], [39, 42, 457], [38, 34, 469], [46, 33, 482], [50, 40, 495], [53, 42, 507], [51, 47, 519], [53, 47, 531], [58, 43, 544], [65, 43, 557], [69, 50, 570], [70, 51, 582], [77, 32, 597], [103, 23, 612], [106, 23, 621], [112, 24, 630], [118, 27, 638], [125, 21, 646], [123, 36, 654], [122, 34, 661], [119, 31, 669], [109, 29, 677], [108, 30, 684], [108, 31, 691], [108, 31, 692], [103, 29, 700], [103, 33, 707], [102, 42, 715], [98, 44, 721], [91, 44, 724], [76, 46, 725], [86, 51, 803], [88, 56, 804], [91, 70, 806], [85, 72, 808], [81, 71, 810], [78, 67, 812], [81, 64, 815], [79, 62, 818], [61, 68, 825], [67, 71, 834], [71, 73, 843], [71, 93, 854], [82, 93, 864], [88, 98, 874], [94, 107, 884], [95, 109, 894], [78, 106, 906], [76, 114, 916], [80, 118, 927], [86, 116, 938], [87, 132, 950], [82, 139, 961], [77, 138, 972], [81, 149, 984], [89, 144, 995]]
+	# print_image(cities,ratio)
+	time2 = time.time()
 	print(som(cities,1500))
+	# print(time.asctime(time.localtime(time.time())))
+
